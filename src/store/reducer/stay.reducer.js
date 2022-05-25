25956a9b233492c92db3f116fbc5347d5a313e00
Denis_Gershovich <denis.gershovich@gmail.com>
2022-05-25 11:46:39 +0300

    added stay store

---
+++ src/store/reducer/stay.reducer.js
@@ -0,0 +1,16 @@
+import {stayService} from '../../services/stay.service.js'
+
+
+const initialState = {
+    stays:null,
+    currStay:null    
+}
+
+export function stayReducer(state = initialState, action) {
+    var newState = state;
+    switch (action.type) {
+   
+        
+    }
+    return newState;
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import {userReducer} from './reducer/user.reducer.js'
+import {stayReducer} from './reducer/stay.reducer.js'
 
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 
 const rootReducer = combineReducers({
-    userModule:userReducer
+    userModule:userReducer,
+    stayModule:stayReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
