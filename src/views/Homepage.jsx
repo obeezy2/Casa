84ee07488e043cd66bcea7d2db316a5cff36143e
Ariel Broukman <102252677+obeezy2@users.noreply.github.com>
2022-05-25 19:53:53 +0300

    push

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Hero } from './hero.jsx'
+import { StayList } from "../cmps/stay-list.jsx";
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { loadStays } from "../store/action/stay.action.js";
 export const Homepage = () => {
+    const { stays } = useSelector((storeState) => storeState.stayModule)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(loadStays(null))
+    }, [])
+
+
     return <section>
         <Hero />
-
+        <StayList stays={stays} />
     </section>
 }
