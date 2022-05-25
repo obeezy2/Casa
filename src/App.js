94498e2a7c61403d50dd01c4b609fd7a0474ce5e
Uri Baynesay <uri552@gmail.com>
2022-05-25 13:52:45 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import '../src/assets/scss/main.scss';
-import {Homepage} from './views/Homepage.jsx'
+import { Link, Routes, Route } from "react-router-dom";
+
+import "../src/assets/scss/main.scss";
+import routes from "./routes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <h1>
-         Aircasa
-        </h1>
-      <div className='nav'>
-        <p>home</p>
-        <p>explore</p>
-        <p>host</p>
-        <p>login</p>
-      </div>  
+        <Link to="/">
+          <h1>Aircasa</h1>
+        </Link>
+        <nav>
+          <Link to="/stays">Explore</Link>
+          <Link to="/host">Become a host</Link>
+          <Link to="/login">Login</Link>
+        </nav>
       </header>
-      <Homepage /> 
+      <main>
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={true}
+                element={route.component}
+              />
+            );
+          })}
+        </Routes>
+      </main>
+
       <footer>
         <h4>footer</h4>
       </footer>
