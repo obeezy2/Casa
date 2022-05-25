d2c23a65354c2b9fc87b8ddbd7bd2901e1012323
Denis_Gershovich <denis.gershovich@gmail.com>
2022-05-25 12:02:00 +0300

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import '../src/assets/scss/main.scss';
+import {Homepage} from './views/Homepage.jsx'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello
-        </p>
-
+         <h1>
+         Aircasa
+        </h1>
+      <div className='nav'>
+        <p>home</p>
+        <p>explore</p>
+        <p>host</p>
+        <p>login</p>
+      </div>  
       </header>
+      <Homepage /> 
     </div>
   );
 }
---
+++ src/views/Homepage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Homepage = () => {
+
+    return <section>
+        <h1>Home page</h1>
+    </section>
+}
