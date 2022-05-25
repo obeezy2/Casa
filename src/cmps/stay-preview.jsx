d9cf1345f81e0ed9ebda9b4b14d2950bc967bba7
Ariel Broukman <102252677+obeezy2@users.noreply.github.com>
2022-05-25 13:19:20 +0300

    update

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -6,13 +6,13 @@ export function StayPreview({ stay, remove }) {
         <div className="stay-img-container">
         </div>
         <h2 className='stay-name'>{stay.name}</h2>
-        <p className="stay-price">${stay.price}</p>
+        <p className="stay-price">${stay}</p>
         <div className="preview-buttons">
             <div onClick={() => remove(stay.id)} className='remove-stay-btn'></div>
-            <Link to={`/stay/edit/${stay._id}`}>
+            <Link to={`/stay/edit/${stay.id}`}>
                 <div className='edit-stay-btn'></div>
             </Link>
-            <Link to={`/stay/details/${stay._id}`}>
+            <Link to={`/stay/details/${stay.id}`}>
                 <div className='details-stay-btn'></div>
             </Link>
         </div>
