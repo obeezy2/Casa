d1631d18f073e370a1e86d3ac4e91bbb83ea78d0
Ariel Broukman <102252677+obeezy2@users.noreply.github.com>
2022-05-25 13:23:54 +0300

    update

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -1,7 +1,7 @@
 
 import { Link } from "react-router-dom"
 
-export function StayPreview({ stay, remove }) {
+export function StayPreview({ stay }) {
     return <div className="stay-preview">
         <div className="stay-img-container">
         </div>
