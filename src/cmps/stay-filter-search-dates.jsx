5bd6595ba36bed8175104b8f9d79b96019e6d95b
Denis_Gershovich <denis.gershovich@gmail.com>
2022-05-26 16:22:26 +0300

    added filter expand text color

--- src/cmps/stay-filter-search-dates.jsx
+++ src/cmps/stay-filter-search-dates.jsx
@@ -5,9 +5,9 @@ import 'react-dates/lib/css/_datepicker.css';
 
 export function SearchByDate(){
   const [date,setDate] = useState({startDate:null,endDate:null})
-  console.log(date)
   return <div>
-<DateRangePicker
+  <h2>choose dates</h2>
+ <DateRangePicker
   startDate={date.startDate} // momentPropTypes.momentObj or null,
   startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
   endDate={date.endDate} // momentPropTypes.momentObj or null,
@@ -15,6 +15,6 @@ export function SearchByDate(){
   onDatesChange={({ startDate, endDate }) => setDate({ startDate, endDate })} // PropTypes.func.isRequired,
   focusedInput={date.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
   onFocusChange={focusedInput => setDate({ focusedInput })} // PropTypes.func.isRequired,
-/>
+ />
   </div>
 }
