7e398fe880543a87c78e8e257465ecbf94913ad2
Denis_Gershovich <denis.gershovich@gmail.com>
2022-05-26 20:28:26 +0300

    changed search dates to class component

--- src/cmps/stay-filter-search-dates.jsx
+++ src/cmps/stay-filter-search-dates.jsx
@@ -3,18 +3,24 @@ import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
-export function SearchByDate(){
-  const [date,setDate] = useState({startDate:null,endDate:null})
-  return <div>
+export class SearchByDate extends React.Component{
+  
+  state = {
+    startDate:null,
+    endDate:null
+  }
+  
+  render (){
+  return<div>
   <h2>choose dates</h2>
- <DateRangePicker
-  startDate={date.startDate} // momentPropTypes.momentObj or null,
+  <DateRangePicker
+  startDate={this.state.startDate} // momentPropTypes.momentObj or null,
   startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-  endDate={date.endDate} // momentPropTypes.momentObj or null,
+  endDate={this.state.endDate} // momentPropTypes.momentObj or null,
   endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-  onDatesChange={({ startDate, endDate }) => setDate({ startDate, endDate })} // PropTypes.func.isRequired,
-  focusedInput={date.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-  onFocusChange={focusedInput => setDate({ focusedInput })} // PropTypes.func.isRequired,
- />
-  </div>
+  onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+  focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+  onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+  />
+  </div>}
 }
