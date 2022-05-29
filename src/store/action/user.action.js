f0e960670ab992b9ad562800b85d2d0d8a7eb6c6
Uri Baynesay <uri552@gmail.com>
2022-05-29 13:17:32 +0300

    change the action for user

--- src/store/action/user.action.js
+++ src/store/action/user.action.js
@@ -1,45 +1,44 @@
-import { userService } from '../../services/user.service'
+import { userService } from "../../services/user.service";
 
 export function onLogin(credentials) {
+  return async (dispatch) => {
     try {
-        return async (dispatch) => {
-            const user = await userService.login(credentials)
-
-            dispatch({
-                type: 'SET_USER',
-                user,
-            })
-        }
+      const user = await userService.login(credentials);
+      dispatch({
+        type: "SET_USER",
+        user,
+      });
     } catch (err) {
-        console.log('Failed to login', err)
+      console.error(err);
     }
+  };
 }
 
 export function onLogout() {
-    try {
-        return async (dispatch) => {
-            await userService.logout()
+  try {
+    return async (dispatch) => {
+      await userService.logout();
 
-            dispatch({
-                type: 'SET_USER',
-                user: null,
-            })
-        }
-    } catch (err) {
-        console.log('Failed to load logout', err)
-    }
+      dispatch({
+        type: "SET_USER",
+        user: null,
+      });
+    };
+  } catch (err) {
+    console.log("Failed to load logout", err);
+  }
 }
 export function onSignup(newUser) {
-    try {
-        return async (dispatch) => {
-            const user = await userService.signup(newUser)
+  try {
+    return async (dispatch) => {
+      const user = await userService.signup(newUser);
 
-            dispatch({
-                type: 'SET_USER',
-                user: user,
-            })
-        }
-    } catch (err) {
-        console.log('Failed to signup', err)
-    }
+      dispatch({
+        type: "SET_USER",
+        user: user,
+      });
+    };
+  } catch (err) {
+    console.log("Failed to signup", err);
+  }
 }
