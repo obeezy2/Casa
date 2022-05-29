7c3eb71d2f2d91c139f383eead5df7267134072f
Ariel Broukman <102252677+obeezy2@users.noreply.github.com>
2022-05-29 15:21:52 +0300

    upadte

--- src/cmps/stay-filter.jsx
+++ src/cmps/stay-filter.jsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import { SearchByDate } from './stay-filter-search-dates'
 import { AddGuestsFilter } from './stay-filter-addGuest-filter'
 import worldLogo from '../assets/img/filter/world.jpg'
-import { useDispatch } from 'react-redux'
 import { setFilterByAction } from '../store/action/stay.action.js'
 import { useNavigate } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import { useDispatch } from 'react-redux'
-import { setFilterByAction } from '../store/action/stay.action.js'
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const StayFilter = () => {
 
@@ -16,7 +14,6 @@ export const StayFilter = () => {
   const [currExpand, setExpand] = useState(null)
   const [filterBy, setFilterBy] = useState({})
   const dispatch = useDispatch()
-  let navigate = useNavigate()
   let navigate = useNavigate()
   let location = useLocation();
 
@@ -34,84 +31,85 @@ export const StayFilter = () => {
   //   window.addEventListener('click',()=>{
   //   })
   // },[])
-}
 
-useEffect(() => {
-  //close filter expand when moveing to another page 
-  if (location.pathname !== '/') {
-    setFilterExpand(false)
-  }
-  //use event on document body to close filter expand 
-  document.querySelector('.main-content').addEventListener('click', () => {
-    setFilterExpand(false)
-  })
 
-}, [location])
+  useEffect(() => {
+    //close filter expand when moveing to another page 
+    if (location.pathname !== '/') {
+      setFilterExpand(false)
+    }
+    //use event on document body to close filter expand 
+    document.querySelector('.main-content').addEventListener('click', () => {
+      setFilterExpand(false)
+    })
 
-return <section className="app-filter-container">
-  <div className="app-filter">
-    <div className='filter-btn-container filter-btn-location' onClick={() => {
-      setFilterExpand(!isFilterExpand)
-      setExpand('Anywhere')
-    }}>
-      <div className="filter-btn" >
-        {currExpand === 'Anywhere' && isFilterExpand ?
-          <div>
-            Where
-            <form>
-              <input className="destination-input" type="text"
-                onClick={(e) => e.stopPropagation()}
-                onChange={(event) => { onChangeFilter(event) }}
-                placeholder='search destination' />
-            </form>
-          </div>
-          : 'Anywhere'}
-      </div>
-    </div>
-    <span className="filter-span"></span>
-    <div className='filter-btn-container filter-btn-dates' onClick={() => {
-      setFilterExpand(!isFilterExpand)
-      setExpand('Any week')
-    }}>
-      <div className="filter-btn" >
-        {currExpand === 'Any week' && isFilterExpand ?
-          <div>
-            <p>When</p>
-            <p>Any week</p>
-          </div>
-          : 'Any week'}
-      </div>
-    </div>
-    <span className="filter-span"></span>
-    <div className='filter-btn-container filter-btn-guests' >
-      <div className="filter-btn" onClick={() => {
+  }, [location])
+
+  return (<section className="app-filter-container">
+    <div className="app-filter">
+      <div className='filter-btn-container filter-btn-location' onClick={() => {
         setFilterExpand(!isFilterExpand)
-        setExpand('Add guests')
+        setExpand('Anywhere')
       }}>
-        {currExpand === 'Add guests' && isFilterExpand ?
-          <div>
-            <p>Who</p>
-            <p>Add guests</p>
-          </div>
-          : 'Add guests'}
+        <div className="filter-btn" >
+          {currExpand === 'Anywhere' && isFilterExpand ?
+            <div>
+              Where
+              <form>
+                <input className="destination-input" type="text"
+                  onClick={(e) => e.stopPropagation()}
+                  onChange={(event) => { onChangeFilter(event) }}
+                  placeholder='search destination' />
+              </form>
+            </div>
+            : 'Anywhere'}
+        </div>
       </div>
-      <div className="search">
-        <div className="search-icon" onClick={() => onSetFilter()}><SearchIcon fontSize="medium" /></div>
+      <span className="filter-span"></span>
+      <div className='filter-btn-container filter-btn-dates' onClick={() => {
+        setFilterExpand(!isFilterExpand)
+        setExpand('Any week')
+      }}>
+        <div className="filter-btn" >
+          {currExpand === 'Any week' && isFilterExpand ?
+            <div>
+              <p>When</p>
+              <p>Any week</p>
+            </div>
+            : 'Any week'}
+        </div>
+      </div>
+      <span className="filter-span"></span>
+      <div className='filter-btn-container filter-btn-guests' >
+        <div className="filter-btn" onClick={() => {
+          setFilterExpand(!isFilterExpand)
+          setExpand('Add guests')
+        }}>
+          {currExpand === 'Add guests' && isFilterExpand ?
+            <div>
+              <p>Who</p>
+              <p>Add guests</p>
+            </div>
+            : 'Add guests'}
+        </div>
+        <div className="search">
+          <div className="search-icon" onClick={() => onSetFilter()}><SearchIcon fontSize="medium" /></div>
+        </div>
       </div>
     </div>
-  </div>
-  {isFilterExpand && <div className="filter-expand">
-    {currExpand === 'Anywhere' && <div>
-      <SearchByDestination setRegionFilter={(region) => setFilterBy({ ...filterBy, region })} />
+    {isFilterExpand && <div className="filter-expand">
+      {currExpand === 'Anywhere' && <div>
+        <SearchByDestination setRegionFilter={(region) => setFilterBy({ ...filterBy, region })} />
+      </div>}
+      {currExpand === 'Any week' && <div>
+        <SearchByDate onSetDates={(start, end) => setFilterBy({ ...filterBy, startDate: start, endDate: end })} />
+      </div>}
+      {currExpand === 'Add guests' && <div>
+        <AddGuestsFilter setGuests={(guests) => setFilterBy({ ...filterBy, guestsNumber: guests })} />
+      </div>}
     </div>}
-    {currExpand === 'Any week' && <div>
-      <SearchByDate onSetDates={(start, end) => setFilterBy({ ...filterBy, startDate: start, endDate: end })} />
-    </div>}
-    {currExpand === 'Add guests' && <div>
-      <AddGuestsFilter setGuests={(guests) => setFilterBy({ ...filterBy, guestsNumber: guests })} />
-    </div>}
-  </div>}
-</section>
+  </section>
+  )
 }
 
 
@@ -119,30 +117,24 @@ function SearchByDestination(props) {
   const [region, setRegion] = useState('')
   useEffect(() => {
     if (region === '') return
-    function SearchByDestination(props) {
-      const [region, setRegion] = useState('')
-      useEffect(() => {
-        if (region === '') return
-        props.setRegionFilter(region)
-      }, [region])
-      return <div className="destination-search-container">
-        <h4 className="destination-search-container-header">search by region</h4>
-        <div className="regions-container">
-          <Destination logo={worldLogo} region={'flexible'} setRegion={setRegion} />
-          <Destination logo={worldLogo} region={'United States'} setRegion={setRegion} />
-          <Destination logo={worldLogo} region={'Middle East'} setRegion={setRegion} />
-          <Destination logo={worldLogo} region={'France'} setRegion={setRegion} />
-          <Destination logo={worldLogo} region={'South America'} setRegion={setRegion} />
-          <Destination logo={worldLogo} region={'Italy'} setRegion={setRegion} />
-        </div>
-      </div>
-    }
-
-    function Destination(props) {
-      return <div className="region-image-container" onClick={() => props.setRegion(props.region)}>
-        <img src={props.logo} alt="err" className="region-image" />
-        <p>{props.region}</p>
-      </div>
-    }
-
+    props.setRegionFilter(region)
+  }, [region])
+  return <div className="destination-search-container">
+    <h4 className="destination-search-container-header">search by region</h4>
+    <div className="regions-container">
+      <Destination logo={worldLogo} region={'flexible'} setRegion={setRegion} />
+      <Destination logo={worldLogo} region={'United States'} setRegion={setRegion} />
+      <Destination logo={worldLogo} region={'Middle East'} setRegion={setRegion} />
+      <Destination logo={worldLogo} region={'France'} setRegion={setRegion} />
+      <Destination logo={worldLogo} region={'South America'} setRegion={setRegion} />
+      <Destination logo={worldLogo} region={'Italy'} setRegion={setRegion} />
+    </div>
+  </div>
+}
+function Destination(props) {
+  return <div className="region-image-container" onClick={() => props.setRegion(props.region)}>
+    <img src={props.logo} alt="err" className="region-image" />
+    <p>{props.region}</p>
+  </div>
+}
 
