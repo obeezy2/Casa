8e2ff3722f063cff926cb852a40bfc8597fdc901
Uri Baynesay <uri552@gmail.com>
2022-06-03 12:00:12 +0300

    added the local storage back

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,11 @@
 // import { user_db } from "../data/db";
 import { httpService } from "./http.service"
 
-const END_POINT_AUTH = { signup: "auth/signup", login: "auth/login", logout: "auth/logout" }
+const END_POINT_AUTH = {
+  signup: "auth/signup",
+  login: "auth/login",
+  logout: "auth/logout",
+}
 const END_POINT_USER = "user"
 
 const STORAGE_KEY_LOGGEDIN_USER = "loggedInUser"
@@ -29,14 +33,21 @@ export const userService = {
 
 async function getUsers() {
   return await httpService.get(END_POINT_USER)
+
+  //return storageService.query(STORAGE_KEY)
 }
 
 async function getById(userId) {
   return await httpService.get(END_POINT_USER, userId)
+
+  //   const user = await storageService.get('user', userId)
+  //     return user;
 }
 
 async function remove(userId) {
   return await httpService.delete(`${END_POINT_USER}/${userId}`)
+
+  // return storageService.remove('user', userId)
 }
 
 async function update(user) {
@@ -56,6 +67,15 @@ async function login(userCred) {
   } catch (err) {
     console.log("Failed to login", err.msg)
   }
+
+  //    try {
+  //         const users = await storageService.query(STORAGE_KEY)
+  //         const user = users.find(user => user.username === userCred.username && user.password === userCred.password)
+  //         sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+  //         return user
+  //     } catch (err) {
+  //         console.log('Failed to login', err);
+  //     }
 }
 
 // need username fullname and password
@@ -68,17 +88,26 @@ async function signup(userCred) {
   } catch (err) {
     console.log("Failed to signup", err.msg)
   }
+
+  //   try {
+  //         const user = await storageService.post(STORAGE_KEY, userCred)
+  //         sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+  //         return user
+  //     } catch (err) {
+  //         console.log('Failed to signup', err);
+  //     }
 }
 
 async function logout() {
   await httpService.post(END_POINT_AUTH.logout)
   sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
-}
+
+  //   sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
 
 // function _saveLocalUser(user) {
 //     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
 //     return user
-// }
+}
 
 function getLoggedinUser() {
   return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER)) || null
